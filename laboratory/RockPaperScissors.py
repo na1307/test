228d88ae4c81d7c44c23e3ca13015cc4849e8109
na1307 <na13072017@hotmail.com>
2022-06-17 19:44:16 +0900

    Update RockPaperScissors.py

--- laboratory/RockPaperScissors.py
+++ laboratory/RockPaperScissors.py
@@ -20,11 +20,27 @@ def main():
         print("0. 끝내기")
 
         try:
-            yourChoiceNum = int(input("\n당신의 선택은? : "))
-            if yourChoiceNum == 0: break
-            yourChoice = toGBB(yourChoiceNum)
-            computersChoiceNum = ranr(1,4)
-            computersChoice = toGBB(computersChoiceNum)
+            yourChoice = int(input("\n당신의 선택은? : "))
+            if yourChoice == 0: break
+            computersChoice = ranr(1,4)
+
+            print()
+            print("당신의 선택은 " + toGBB(yourChoice) + " 입니다.")
+            print("컴퓨터의 선택은 " + toGBB(computersChoice) + " 입니다.")
+            print()
+
+            if yourChoice == computersChoice:
+                print("비겼습니다!")
+            elif yourChoice == 3 and computersChoice == 1:
+                print("컴퓨터가 이겼습니다!")
+            elif yourChoice == 1 and computersChoice == 3:
+                print("당신이 이겼습니다!")
+            elif yourChoice < computersChoice:
+                print("컴퓨터가 이겼습니다!")
+            else:
+                print("당신이 이겼습니다!")
+
+            print()
         except NotInRange as e:
             print(e)
             continue
@@ -33,24 +49,6 @@ def main():
             continue
         except Exception as e2:
             raise e2
-
-        print()
-        print("당신의 선택은 " + yourChoice + " 입니다.")
-        print("컴퓨터의 선택은 " + computersChoice + " 입니다.")
-        print()
-
-        if yourChoiceNum == computersChoiceNum:
-            print("비겼습니다!")
-        elif yourChoiceNum == 3 and computersChoiceNum == 1:
-            print("컴퓨터가 이겼습니다!")
-        elif yourChoiceNum == 1 and computersChoiceNum == 3:
-            print("당신이 이겼습니다!")
-        elif yourChoiceNum < computersChoiceNum:
-            print("컴퓨터가 이겼습니다!")
-        else:
-            print("당신이 이겼습니다!")
-
-        print()
 
 if __name__ == "__main__":
     main()
