140a17d9279ff2ddf6145f8539d9bfa7ebbd5ccf
na1307 <na1307@outlook.kr>
2022-11-04 10:38:00 +0900

    TryParse 사용

--- RandomForms/FormMain.Designer.cs
+++ RandomForms/FormMain.Designer.cs
@@ -57,7 +57,7 @@ partial class FormMain {
             this.label2.Location = new System.Drawing.Point(196, 15);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(42, 15);
-            this.label2.TabIndex = 0;
+            this.label2.TabIndex = 2;
             this.label2.Text = "최대 : ";
             // 
             // endBox
@@ -65,7 +65,7 @@ partial class FormMain {
             this.endBox.Location = new System.Drawing.Point(244, 12);
             this.endBox.Name = "endBox";
             this.endBox.Size = new System.Drawing.Size(100, 23);
-            this.endBox.TabIndex = 1;
+            this.endBox.TabIndex = 3;
             // 
             // checkBox1
             // 
@@ -73,7 +73,7 @@ partial class FormMain {
             this.checkBox1.Location = new System.Drawing.Point(389, 14);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(74, 19);
-            this.checkBox1.TabIndex = 2;
+            this.checkBox1.TabIndex = 4;
             this.checkBox1.Text = "중복없음";
             this.checkBox1.UseVisualStyleBackColor = true;
             // 
@@ -82,7 +82,7 @@ partial class FormMain {
             this.button1.Location = new System.Drawing.Point(12, 41);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(460, 50);
-            this.button1.TabIndex = 3;
+            this.button1.TabIndex = 5;
             this.button1.Text = "뽑기";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
@@ -93,7 +93,7 @@ partial class FormMain {
             this.numlabel.Location = new System.Drawing.Point(12, 94);
             this.numlabel.Name = "numlabel";
             this.numlabel.Size = new System.Drawing.Size(460, 100);
-            this.numlabel.TabIndex = 4;
+            this.numlabel.TabIndex = 6;
             this.numlabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // listBox1
@@ -103,14 +103,14 @@ partial class FormMain {
             this.listBox1.Location = new System.Drawing.Point(12, 197);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(460, 199);
-            this.listBox1.TabIndex = 5;
+            this.listBox1.TabIndex = 7;
             // 
             // button2
             // 
             this.button2.Location = new System.Drawing.Point(12, 399);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(460, 50);
-            this.button2.TabIndex = 6;
+            this.button2.TabIndex = 8;
             this.button2.Text = "비우기";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
--- RandomForms/FormMain.cs
+++ RandomForms/FormMain.cs
@@ -8,51 +8,45 @@ public partial class FormMain : Form {
     private void button1_Click(object sender, EventArgs e) {
         if (string.IsNullOrWhiteSpace(startBox.Text) || string.IsNullOrWhiteSpace(endBox.Text)) return;
 
-        try {
-            var minValue = int.Parse(startBox.Text);
-            var maxValue = int.Parse(endBox.Text);
+        if (!int.TryParse(startBox.Text, out var minValue) || !int.TryParse(endBox.Text, out var maxValue)) {
+            ErrMsg("숫자가 아닌 문자열을 입력했거나 숫자가 너무 큽니다.");
+            return;
+        }
 
-            if (minValue < 0) {
-                ErrMsg("최소값은 음수일 수 없습니다.");
-                return;
-            }
+        if (minValue < 0) {
+            ErrMsg("최소값은 음수일 수 없습니다.");
+            return;
+        }
 
-            if (minValue > maxValue) {
-                ErrMsg("최소값은 최대값보다 클 수 없습니다.");
-                return;
-            }
+        if (minValue > maxValue) {
+            ErrMsg("최소값은 최대값보다 클 수 없습니다.");
+            return;
+        }
 
-            maxValue++;
+        maxValue++;
 
-            if (!checkBox1.Checked) {
-                if (ints != null) ints = null;
-
+        if (!checkBox1.Checked) {
+            ints = null;
+            add(Random.Shared.Next(minValue, maxValue));
+        } else {
+            if (ints == null || ints.Capacity != (maxValue - minValue) || ints.Count >= (maxValue - minValue)) {
+                listBox1.Items.Clear();
+                ints = new(maxValue - minValue);
                 add(Random.Shared.Next(minValue, maxValue));
             } else {
-                if (ints == null || (ints.Capacity == (maxValue - minValue) && ints.Count >= (maxValue - minValue)) || ints.Capacity != (maxValue - minValue)) {
-                    listBox1.Items.Clear();
-                    ints = new(maxValue - minValue);
-                    add(Random.Shared.Next(minValue, maxValue));
-                } else {
-                    int value;
+                int value;
 
-                    do {
-                        value = Random.Shared.Next(minValue, maxValue);
-                    } while (ints.Contains(value));
+                do {
+                    value = Random.Shared.Next(minValue, maxValue);
+                } while (ints.Contains(value));
 
-                    add(value);
-                }
+                add(value);
             }
-        } catch (FormatException) {
-            ErrMsg("숫자만 입력하세요.");
-        } catch (OverflowException) {
-            ErrMsg("숫자가 너무 큽니다.");
         }
 
         void add(int value) {
             numlabel.Text = value.ToString();
             listBox1.Items.Add(value);
-
             if (ints != null) ints.Add(value);
         }
     }
@@ -60,7 +54,6 @@ public partial class FormMain : Form {
     private void button2_Click(object sender, EventArgs e) {
         numlabel.Text = null;
         listBox1.Items.Clear();
-
         if (ints != null) ints = null;
     }
 }
