641a0c74e12e9f7a2917a992633ce4dfde73ddd5
Jonghyo Lee <na1307@outlook.kr>
2024-05-28 15:45:54 +0900

    Add 2 projects

---
+++ Dotnet4VersionDetector/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Win32;
+
+const string regpath = @"SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full";
+
+using var reg = Registry.LocalMachine.OpenSubKey(regpath);
+
+MessageBox.Show(".NET Framework 4." + reg.GetValue("Release") switch {
+    >= 533320 => "8.1",
+    >= 528040 => "8",
+    >= 461808 => "7.2",
+    >= 461308 => "7.1",
+    >= 460798 => "7",
+    >= 394802 => "6.2",
+    >= 394254 => "6.1",
+    >= 393295 => "6",
+    >= 379893 => "5.2",
+    >= 378675 => "5.1",
+    >= 378389 => "5",
+    _ => "0"
+});
---
+++ I386Duplicates/Program.cs
@@ -0,0 +1,40 @@
+﻿try {
+    var dir = args.Length switch {
+        0 => Directory.GetCurrentDirectory(),
+        1 => args[0],
+        _ => throw new ArgumentException("Too Many Arguments."),
+    };
+
+    output(dir + "\\I386");
+
+    if (Directory.Exists(dir + "\\AMD64")) {
+        output(dir + "\\AMD64");
+    }
+} catch (Exception ex) {
+    Console.WriteLine(ex.Message);
+    Environment.Exit(ex.HResult);
+}
+
+static void output(string dirname) {
+    dirname = Path.GetFullPath(dirname);
+
+    if (!Directory.Exists(dirname)) {
+        throw new DirectoryNotFoundException($"\"{dirname}\" Directory not found.");
+    }
+
+    Console.WriteLine("Duplicate files in " + dirname + " directory : ");
+
+    var files = Directory.GetFiles(dirname);
+    var duplicates = files.Where(predicate).Select(selector).ToList();
+
+    duplicates.ForEach(Console.WriteLine);
+
+    Console.WriteLine();
+
+    Console.WriteLine("Total number of files : " + files.Length);
+    Console.WriteLine("Number of duplicate files : " + duplicates.Count);
+}
+
+static string cfn(string fn) => !string.IsNullOrEmpty(Path.GetExtension(fn)) ? fn[..^1] + "_" : fn + "._";
+static bool predicate(string fn) => fn[^1] != '_' && File.Exists(cfn(fn));
+static string selector(string fn) => Path.GetFileName(cfn(fn));
