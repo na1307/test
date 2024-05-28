4f9271d0fb34c77b734f43a458a7cebc5394197f
Jonghyo Lee <na1307@outlook.kr>
2024-05-28 17:52:28 +0900

    test

---
+++ GpgStarter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+
+await run("gpg-agent");
+await run("keyboxd");
+
+async Task run(string processName) {
+    var attempt = 0;
+    int code;
+
+    do {
+        attempt++;
+        Console.Write($"Starting {processName} (Attempt {attempt})...");
+
+        using Process gpgconf = new() {
+            StartInfo = new() {
+                FileName = "gpgconf.exe",
+                Arguments = $"--launch {processName}",
+                UseShellExecute = false,
+                CreateNoWindow = true,
+            }
+        };
+
+        gpgconf.Start();
+        await gpgconf.WaitForExitAsync();
+        code = gpgconf.ExitCode;
+        Console.WriteLine();
+    } while (code != 0);
+}
