8840b5bcf6f86dd1579ee0b0bb181561da132866
Jonghyo Lee <na1307@outlook.kr>
2025-07-10 23:41:22 +0900

    App1

--- ComTest/ComTest/IExplorerCommand.cs
+++ ComTest/ComTest/IExplorerCommand.cs
@@ -1,7 +1,60 @@
-﻿using System.Runtime.InteropServices;
+﻿using Bluehill.NativeCom;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.Marshalling;
 
 namespace ComTest;
+
+public enum Sigdn {
+    Normaldisplay = 0,
+    Parentrelativeparsing = unchecked((int)0x80018001),
+    Desktopabsoluteparsing = unchecked((int)0x80028000),
+    Parentrelativeediting = unchecked((int)0x80031001),
+    Desktopabsoluteediting = unchecked((int)0x8004c000),
+    Filesyspath = unchecked((int)0x80058000),
+    Url = unchecked((int)0x80068000),
+    Parentrelativeforaddressbar = unchecked((int)0x8007c001),
+    Parentrelative = unchecked((int)0x80080001),
+    Parentrelativeforui = unchecked((int)0x80094001)
+}
+
+[GeneratedComInterface]
+[Guid("43826d1e-e718-42ee-bc55-a1e261c37bfe")]
+[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+public unsafe partial interface IShellItem {
+    void* BindToHandler(void* bc, in Guid bhid, in Guid iid);
+
+    IShellItem? GetParent();
+
+    [return: MarshalAs(UnmanagedType.LPWStr)]
+    string GetDisplayName(Sigdn sigdnName);
+
+    [PreserveSig]
+    int GetAttributes(uint sfgaoMask, out uint sfgaoAttribs);
+
+    [PreserveSig]
+    int Compare(IShellItem si, uint hint, out int order);
+}
+
+[GeneratedComInterface]
+[Guid("b63ea76d-1f85-456f-a19c-48159efa858b")]
+[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+public unsafe partial interface IShellItemArray {
+    void* BindToHandler(void* bc, in Guid bhid, in Guid iid);
+
+    void* GetPropertyStore(int flags, in Guid iid);
+
+    void* GetPropertyDescriptionList(void* keyType, in Guid iid);
+
+    [PreserveSig]
+    int GetAttributes(int attribFlags, uint sfgaoMask, out uint sfgaoAttribs);
+
+    uint GetCount();
+
+    IShellItem? GetItemAt(uint index);
+
+    void* EnumItems();
+}
 
 [Flags]
 public enum Expcmdstate : uint {
@@ -33,107 +86,85 @@ public enum Expcmdflags : uint {
 [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
 public unsafe partial interface IExplorerCommand {
     [return: MarshalAs(UnmanagedType.LPWStr)]
-    string? GetTitle(void* itemArray);
+    string? GetTitle(IShellItemArray? itemArray);
 
     [return: MarshalAs(UnmanagedType.LPWStr)]
-    string? GetIcon(void* itemArray);
+    string? GetIcon(IShellItemArray? itemArray);
 
     [return: MarshalAs(UnmanagedType.LPWStr)]
-    string? GetToolTip(void* itemArray);
+    string? GetToolTip(IShellItemArray? itemArray);
 
     Guid GetCanonicalName();
 
-    Expcmdstate GetState(void* itemArray, [MarshalAs(UnmanagedType.Bool)] bool okToBeSlow);
+    Expcmdstate GetState(IShellItemArray? itemArray, [MarshalAs(UnmanagedType.Bool)] bool okToBeSlow);
 
-    void Invoke(void* itemArray, void* bc);
+    void Invoke(IShellItemArray? itemArray, void* bc);
 
     Expcmdflags GetFlags();
 
-    void** EnumSubCommands();
+    void* EnumSubCommands();
 }
 
 [GeneratedComClass]
 [Guid("059F8A2D-271B-415E-9267-18B9E4B164DC")]
 public sealed unsafe partial class TheCommand : IExplorerCommand {
-    public string? GetTitle(void* itemArray) => "TestCommand";
+    public string? GetTitle(IShellItemArray? itemArray) => "TestCommand";
 
-    public string? GetIcon(void* itemArray) => throw new NotImplementedException();
+    public string? GetIcon(IShellItemArray? itemArray) => throw new NotImplementedException();
 
-    public string? GetToolTip(void* itemArray) => throw new NotImplementedException();
+    public string? GetToolTip(IShellItemArray? itemArray) => throw new NotImplementedException();
 
     public Guid GetCanonicalName() => throw new NotImplementedException();
 
-    public Expcmdstate GetState(void* itemArray, bool okToBeSlow) => Expcmdstate.Enabled;
+    public Expcmdstate GetState(IShellItemArray? itemArray, bool okToBeSlow) => Expcmdstate.Enabled;
 
-    public void Invoke(void* itemArray, void* bc) => MessageBoxW(IntPtr.Zero, "Hello, World!", string.Empty, 64);
+    public void Invoke(IShellItemArray? itemArray, void* bc) {
+        var mh = GetModuleHandleW("ComTest.dll");
+
+        if (mh is null) {
+            MessageBoxW(null, $"GetModuleHandleW Failed: {Marshal.GetLastPInvokeErrorMessage()}", null, 16);
+
+            return;
+        }
+
+        const uint size = byte.MaxValue;
+        var sb = stackalloc char[(int)size];
+
+        if (GetModuleFileNameW(mh, sb, size) == 0) {
+            MessageBoxW(null, $"GetModuleFileNameW Failed: {Marshal.GetLastPInvokeErrorMessage()}", null, 16);
+
+            return;
+        }
+
+        var displayName = itemArray?.GetItemAt(0)?.GetDisplayName(Sigdn.Desktopabsoluteediting);
+
+        if (displayName is null) {
+            MessageBoxW(null, "File name", null, 16);
+
+            return;
+        }
+
+        var p = Process.Start(Path.Combine(Path.GetDirectoryName(new(sb))!, "App1.exe"), displayName);
+
+        if (p is null) {
+            MessageBoxW(null, "App1 launch failed.", null, 16);
+        }
+    }
 
     public Expcmdflags GetFlags() => Expcmdflags.Default;
 
-    public void** EnumSubCommands() => throw new NotImplementedException();
+    public void* EnumSubCommands() => throw new NotImplementedException();
 
     [DllImport("user32.dll", CharSet = CharSet.Unicode, ExactSpelling = true, SetLastError = true)]
-    private static extern int MessageBoxW(IntPtr hwnd, [MarshalAs(UnmanagedType.LPWStr)] string text, [MarshalAs(UnmanagedType.LPWStr)] string caption, uint type);
-}
+    private static extern int MessageBoxW(void* hwnd, [MarshalAs(UnmanagedType.LPWStr)] string? text, [MarshalAs(UnmanagedType.LPWStr)] string? caption, uint type);
 
-[GeneratedComInterface]
-[Guid("00000001-0000-0000-C000-000000000046")]
-[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-public unsafe partial interface IClassFactory {
-    [PreserveSig]
-    int CreateInstance(void* pUnkOuter, Guid* riid, void** ppvObject);
+    [DllImport("kernel32.dll", CharSet = CharSet.Unicode, ExactSpelling = true, SetLastError = true)]
+    private static extern uint GetModuleFileNameW(void* module, char* filename, uint size);
 
-    [PreserveSig]
-    int LockServer([MarshalAs(UnmanagedType.Bool)] bool fLock);
+    [DllImport("kernel32.dll", CharSet = CharSet.Unicode, ExactSpelling = true, SetLastError = true)]
+    private static extern void* GetModuleHandleW([MarshalAs(UnmanagedType.LPWStr)] string moduleName);
 }
 
 [GeneratedComClass]
-[Guid("3B545406-898A-4FC7-8D87-A9E84D72CFBC")]
-public unsafe partial class ClassFactory : IClassFactory {
-    private static readonly Guid IecIid = Guid.Parse("a08ce4d0-fa25-44ab-b57c-c7b1c323e0b9");
-    private static volatile int ServerLocks;
-
-    public static bool IsLocked => ServerLocks > 0;
-
-    public int CreateInstance(void* pUnkOuter, Guid* riid, void** ppvObject) {
-        if (*riid != IecIid) {
-            return unchecked((int)0x80040111); // CLASS_E_CLASSNOTAVAILABLE
-        }
-
-        StrategyBasedComWrappers sbcw = new();
-        TheCommand tc = new();
-
-        var ptr = sbcw.GetOrCreateComInterfaceForObject(tc, CreateComInterfaceFlags.None);
-
-        if (ptr == IntPtr.Zero) {
-            return unchecked((int)0x8000FFFF); // E_UNEXPECTED
-        }
-
-        try {
-            var hr = Marshal.QueryInterface(ptr, in *riid, out var pInterface);
-
-            if (hr != 0) {
-                return hr;
-            }
-
-            if (pInterface == IntPtr.Zero) {
-                return unchecked((int)0x8000FFFF); // E_UNEXPECTED
-            }
-
-            *ppvObject = pInterface.ToPointer();
-
-            return 0; // S_OK
-        } finally {
-            Marshal.Release(ptr);
-        }
-    }
-
-    public int LockServer(bool fLock) {
-        if (fLock) {
-            Interlocked.Increment(ref ServerLocks);
-        } else {
-            Interlocked.Decrement(ref ServerLocks);
-        }
-
-        return 0;
-    }
-}
+[ClassFactory(typeof(TheCommand))]
+internal sealed partial class TheCommandFactory : IClassFactory;
